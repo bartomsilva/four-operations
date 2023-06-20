d5d3f5764610156297abbb964a0f35e18e2262cd
ffernanda85 <f.fernanda85@gmail.com>
2023-06-20 20:29:04 -0300

    Modified: Ajustes código

---
+++ src/endpoints/postSubtraction.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+
+export const postSubtraction = (req: Request, res: Response) => {
+
+    try {
+        
+        const { minuendo, subtraendo } = req.body
+    
+        if (minuendo === undefined || subtraendo === undefined) {
+            res.status(400)
+            throw new Error("enter all the necessary data, try again!");
+
+        } else if (typeof minuendo !== "number" || typeof subtraendo !== "number") {
+            res.status(400)
+            throw new Error("value must be a number, try again!");
+        }
+        const result = minuendo - subtraendo
+        res.status(200).send(`O resultado da subtração: ${minuendo} - ${subtraendo} = ${result}`)
+    } catch (error) {
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Unexpected error")
+        }
+    }
+}
