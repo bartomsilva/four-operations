5c02f8101034d0d380e3efd53562cfe249760245
Regiane Martins <regianegmartins@hotmail.com>
2023-06-19 22:57:07 -0300

    multiplication endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,52 @@
-import express, {Request, Response} from 'express'
-import cors from 'cors'
-const PORT = 3003
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { TMultiplication } from "./types";
+const PORT = 3003;
 
-const server =express()
+const server = express();
 
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
 
-server.listen(3003,()=>console.log("server on in port ", PORT))
+server.listen(3003, () => console.log("server on in port ", PORT));
 
 // vamos trabalhar as 4 operações básicas de matemática coisa simples
 // endpoint tipo post o nome do endpoint é livre um para cada membro
 // adição / subtração / divisão / multiplicação
-// Bart   /   Bruno   / Flávia  / Julia 
-// bora nessa ordem?  
+// Bart   /   Bruno   / Flávia  / Julia
+// bora nessa ordem?
 
+// Endpoint de multiplicação
 
+server.post("/multiplication", (req: Request, res: Response) => {
+  try {
+    const { multiplying, multiplier } = req.body;
 
+    if (multiplying === undefined || multiplier === undefined) {
+      res.statusCode = 400;
+      throw new Error(`Invalid data, please check.`);
+    }
+    if (typeof multiplying !== "number" || typeof multiplier !== "number") {
+      res.statusCode = 400;
+      throw new Error(`Invalid values, please enter numbers!`);
+    }
+
+    const resultMultiplication: TMultiplication = {
+      multiplying,
+      multiplier,
+      result: multiplying * multiplier,
+    };
+
+    res
+      .status(200)
+      .send(
+        `The multiplication of numbers ${multiplying} and ${multiplier} is ${resultMultiplication.result}.`
+      );
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.status(500).send(`Internal server error.`);
+    }
+  }
+});
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type TMultiplication ={
+    multiplying: number,
+    multiplier: number,
+    result: number
+}
